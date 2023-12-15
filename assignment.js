d9a4720565451525990b6ae77e2f1536d853e39b
TodiTheFirst <146870436+TodiTheFirst@users.noreply.github.com>
2023-12-15 15:28:00 +0300

    Update assignment.js

--- assignment.js
+++ assignment.js
@@ -1,4 +1,16 @@
 class Course {
+//5 задание 
+    #price
+// 4 Задание если в любом классе ввести минус в цене он выдаст инвелид вэлью
+  get price() {
+    return "$" + this.#price;
+  }
+  set price(value) {
+    if (value < 0) {
+      throw "Invalid value!";
+    }
+    this.#price = value;
+  }
   constructor(title, lenght, price) {
     this.title = title;
     this.lenght = lenght;
@@ -6,7 +18,7 @@ class Course {
     //2 Задание
   }
   calculation() {
-    return this.lenght / this.price;
+    return this.lenght / this.#price;
   }
   addShortSmurry() {
     console.log(
@@ -24,20 +36,24 @@ class Course {
 // theFirst.addShortSmurry();
 // theSecond.addShortSmurry();
 
+//3 задание
 class PracticalCourse extends Course {
-  constructor(title,price,){
-    super() 
-    this practis = new Course();
-    this.numOfExercises = numOfExercises
+  constructor(title, lenght, price, numOfExercises) {
+    super(title, lenght, price);
+    this.numOfExercises = numOfExercises;
   }
 }
 
 class TheorethicalCourse extends Course {
-    addTextTool(){
-        console.log(`Well it write some text for new class`)
-    }
+  addTextTool() {
+    console.log(`Well it write some text for new class`);
+  }
 }
 
-const practis = 
+const practis = new PracticalCourse("Js", 1000, 4, 20);
+const practisV = new TheorethicalCourse("Course for chill", 200, 50);
 
 console.log(practis);
+practis.addShortSmurry();
+practisV.addTextTool();
+practisV.addShortSmurry();
